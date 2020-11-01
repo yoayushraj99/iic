c6a5f3f8039658493e70b62a1d2f6746c2482919
Ayush Raj <yoayushraj99@gmail.com>
2020-11-01 17:29:27 +0530

    Add About section in Home Page

---
+++ src/components/home/About.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import Divider from "./Divider";
+
+function About() {
+  return (
+    <div className="container">
+      <div className="container">
+        <h1 className="sec-title">About iic</h1>
+        <h5 className="content">
+          Sed vulputate urna vel sem pellentesque finibus. Nunc dictum laoreet
+          ex ac sodales. In ultricies sapien ac quam vestibulum, a laoreet
+          ligula posuere. Nullam id nibh sit amet mi varius molestie porttitor
+          gravida sapien. Nulla est elit, venenatis ac venenatis nec, molestie
+          sit amet elit. Nam semper ut velit in dignissim. Donec sed massa ac
+          felis tempor commodo eu a ipsum. Integer id leo tempus magna molestie
+          accumsan vel sit amet risus.
+        </h5>
+      </div>
+      <Divider />
+    </div>
+  );
+}
+
+export default About;
