1f8be7aab32a14e8f4349201e957f90bdad99386
Ayush Raj <yoayushraj99@gmail.com>
2020-11-01 17:32:48 +0530

    Add Divider component

---
+++ src/components/Dividers/Divider.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Divider() {
+  return (
+    <div>
+      <div className="divider div-transparent div-dot"></div>
+    </div>
+  );
+}
+
+export default Divider;
