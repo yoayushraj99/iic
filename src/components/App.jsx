aaac668e8d19508fbf0fb42175813532f4d31385
Anwer Sayeed <deeyasrewna@gmail.com>
2020-12-04 18:22:45 +0530

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -23,6 +23,7 @@ function App() {
              <Route path="/Portfolio" component={Portfolio}/>
              <Route path="/Programmes" component={Programmes}/>
              <Route path="/Testimonial" component={Testimonial}/>
+              <Route path="/Guideline" component={Guideline}/>
              <Route path="/Tinkering" component={Tinkering}/>
              
             <Route component={Error}/>
