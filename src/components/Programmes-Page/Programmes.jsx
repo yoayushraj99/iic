7d7e4994b1478b812a8515d8be8d94e41ce6a882
anwersayeed <deeyasrewna@gmail.com>
2020-12-05 16:30:17 +0530

    many changes

--- src/components/Programmes-Page/Programmes.jsx
+++ src/components/Programmes-Page/Programmes.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import Navbar from "../Nav-foot/Navbar";
 import Footer from "../Nav-foot/Footer";
+import under_c from '../../assets/img/under_c.png';
 
 
 function Programmes() {
   return (
     <div>
       <Navbar />
-      <h1>Page Under Progress - Programmes</h1>
+      
+      <img className="w-50" src={under_c} style={{marginLeft: "20%"}} alt="Webpage under construction" />
+      
+      
       <Footer />
     </div>
   );
--- src/components/Testimonial-Page/Testimonial.jsx
+++ src/components/Testimonial-Page/Testimonial.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import Navbar from "../Nav-foot/Navbar";
 import Footer from "../Nav-foot/Footer";
-
+import under_c from '../../assets/img/under_c.png';
 
 function Testimonial() {
   return (
     <div>
       <Navbar />
-      <h1>Page Under Progress - Testimonial</h1>
+      <img className="w-50" src={under_c} style={{marginLeft: "20%"}} alt="Webpage under construction" />
       <Footer />
     </div>
   );
