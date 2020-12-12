0b140ba63a84f7ac0620b452ee412f60499d9cfe
iamayushraj <yoayushraj99@gmail.com>
2020-12-12 14:00:26 +0530

    Toggle Button Color Issue Fixed

--- src/components/AshokSen/AshokSen.jsx
+++ src/components/AshokSen/AshokSen.jsx
@@ -15,18 +15,6 @@ function App() {
           <a className="navbar-brand js-scroll-trigger" href="#page-top">
             <NavLink to="/">Back to IIIC</NavLink>
           </a>
-          <button
-            className="navbar-toggler navbar-toggler-right"
-            type="button"
-            data-toggle="collapse"
-            data-target="#navbarResponsive"
-            aria-controls="navbarResponsive"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            Menu
-            <i className="fa fa-bars"></i>
-          </button>
         </div>
       </nav>
 
