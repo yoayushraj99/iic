cb35eec65da815494441a287761daa2213033cb5
iamayushraj <yoayushraj99@gmail.com>
2021-01-13 11:53:51 +0530

    Event Page 1.Created an Event PAge. 2.changed Testimonial section of navbar to Events. 3.Routed events in app.jsx. 4.Added 3 images of Ashok sen in images directory.

---
+++ src/components/Event/Events.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Navbar from "../Nav-foot/Navbar";
+import Footer from "../Nav-foot/Footer";
+import IncubationNAv from "../Nav-foot/IncubationNav";
+
+function Events() {
+  return (
+    <div>
+      <IncubationNAv />
+      <Navbar />
+      <Footer />
+    </div>
+  );
+}
+
+export default Events;
