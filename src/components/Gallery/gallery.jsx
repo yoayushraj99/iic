d0965a55006d3c4ee190404e563390091ff5bc5a
Theseregrets <rishitashaw49@gmail.com>
2021-08-05 23:05:15 +0530

    slider added

--- src/components/Gallery/gallery.jsx
+++ src/components/Gallery/gallery.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "../Nav-foot/Navbar";
 import GalleryBody from "./gallery-body";
+import GallerySlider from "./gallery-slider";
 
 export default function Gallery() {
   return (
     <>
       <Navbar />
+      <GallerySlider />
       <GalleryBody />
     </>
   );
