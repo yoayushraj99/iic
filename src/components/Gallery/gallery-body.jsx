cda2e43c9131029a8162454bb3eb7708a0658f59
Theseregrets <rishitashaw49@gmail.com>
2021-08-08 20:04:21 +0530

    gallery done

--- src/components/Gallery/gallery-body.jsx
+++ src/components/Gallery/gallery-body.jsx
@@ -1,43 +1,85 @@
 /* eslint-disable jsx-a11y/alt-text */
 import React from "react";
-import "photoswipe/dist/photoswipe.css";
-import "photoswipe/dist/default-skin/default-skin.css";
-
-import { Gallery, Item } from "react-photoswipe-gallery";
+import "./gallery-body.css";
 
 export default function GalleryBody() {
   return (
     <>
-      <Gallery>
-        <Item
-          original="https://placekitten.com/1024/768?image=1"
-          thumbnail="https://placekitten.com/80/60?image=1"
-          width="1024"
-          height="768"
-        >
-          {({ ref, open }) => (
-            <img
-              ref={ref}
-              onClick={open}
-              src="https://placekitten.com/80/60?image=1"
-            />
-          )}
-        </Item>
-        <Item
-          original="https://placekitten.com/1024/768?image=2"
-          thumbnail="https://placekitten.com/80/60?image=2"
-          width="1024"
-          height="768"
-        >
-          {({ ref, open }) => (
-            <img
-              ref={ref}
-              onClick={open}
-              src="https://placekitten.com/80/60?image=2"
-            />
-          )}
-        </Item>
-      </Gallery>
+      <div id="gallery" class="container-fluid">
+        <img
+          src="https://source.unsplash.com/1600x1200?nature,"
+          class="img-responsive"
+        />
+        <img
+          src="https://source.unsplash.com/1024x768?nature,"
+          class="img-responsive"
+        />
+        <img
+          src="https://source.unsplash.com/1366x768?nature,"
+          class="img-responsive"
+        />
+        <img
+          src="https://source.unsplash.com/1024x768?nature,"
+          class="img-responsive"
+        />
+
+        <img
+          src="https://source.unsplash.com/1920x1080?nature,"
+          class="img-responsive"
+        />
+        <img
+          src="https://source.unsplash.com/640x360?nature,"
+          class="img-responsive"
+        />
+        <img
+          src="https://source.unsplash.com/320x640?nature,"
+          class="img-responsive"
+        />
+        <img
+          src="https://source.unsplash.com/1200x1600?nature,"
+          class="card img-responsive"
+        />
+        <img
+          src="https://source.unsplash.com/800x600?nature,"
+          class="img-responsive"
+        />
+        <img
+          src="https://source.unsplash.com/600x800?nature,"
+          class="img-responsive"
+        />
+        <img
+          src="https://source.unsplash.com/400x600?nature,"
+          class="img-responsive"
+        />
+        <img
+          src="https://source.unsplash.com/600x400?nature,"
+          class="img-responsive"
+        />
+        <img
+          src="https://source.unsplash.com/1100x1600?nature,"
+          class="img-responsive"
+        />
+        <img
+          src="https://source.unsplash.com/1600x1100?nature,"
+          class="img-responsive"
+        />
+        <img
+          src="https://source.unsplash.com/992x768?nature,"
+          class="img-responsive"
+        />
+        <img
+          src="https://source.unsplash.com/768x992?nature,"
+          class="img-responsive"
+        />
+      </div>
+
+      <div id="myModal" class="modal fade" role="dialog">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-body"></div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
--- src/components/Gallery/gallery.jsx
+++ src/components/Gallery/gallery.jsx
@@ -2,13 +2,22 @@ import React from "react";
 import Navbar from "../Nav-foot/Navbar";
 import GalleryBody from "./gallery-body";
 import GallerySlider from "./gallery-slider";
+import Divider from "../Dividers/Divider";
+import Footer from "../Nav-foot/Footer";
 
 export default function Gallery() {
   return (
     <>
       <Navbar />
       <GallerySlider />
+
+      <h1 className="sec-title" style={{ padding: "10px" }}>
+        Gallery
+      </h1>
+      <Divider />
+
       <GalleryBody />
+      <Footer />
     </>
   );
 }
