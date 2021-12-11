bbe75f4a4e140a6653afac5b3d27af0b40e61caa
rishav-jha-mech <rishav.rj74@gmail.com>
2021-12-12 00:05:16 +0530

    Pagination for events list working

--- src/components/ADMIN/Events/Events.js
+++ src/components/ADMIN/Events/Events.js
@@ -1,25 +1,31 @@
 import React, { useState, useEffect } from "react";
 import axios from 'axios'
 import EventListItem from "./EventListItem";
+import Pagination from '@material-ui/lab/Pagination';
 import "./styles.css";
 import Swal from "sweetalert2";
+
 
 const Events = () => {
     const [loading,setLoading]  = useState(true)
 	const [eventData,setEventData] = useState({})
+	const [noOfEvents,setNoOfEvents] = useState(0)
+	const [currentPage,setCurrentPage] = useState(1)
+
 
     useEffect(() => {
       axios({
         method: 'GET',
-        url: 'http://localhost:4000/events/list'
+        url: `http://localhost:4000/events/list?page=${currentPage}`
       }).then(res =>{
-		setEventData(res.data)
+		setNoOfEvents(res.data.totalEvents)
+		setEventData(res.data.resultArray)
 		setLoading(false)
       }).catch(err =>{
 		  console.error(err)
 		  Swal.fire('Error !',`${err}`,'error')
 	  })
-    }, [loading])
+    }, [loading,currentPage])
 
 	const DeleteEvent = (eventId) =>{
 		Swal.fire({
@@ -51,12 +57,18 @@ const Events = () => {
   return (<>
     <div className="admin-events-container">
         <h2 className="heading p-4">Events List</h2>
-        <div className="control-bar mx-3 my-4">
+        <div className="control-bar mx-3 my-4 d-flex justify-content-between">
           <button className="btn text-white" style={{background:'#1F51FF'}} type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">Sort By</button>
           <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
             <li><a class="dropdown-item" href="#">Newer</a></li>
             <li><a class="dropdown-item" href="#">Older</a></li>
           </ul>
+		  <Pagination
+			count={Math.ceil(noOfEvents / 2)}
+			page={currentPage}
+			onChange={(event, value) => setCurrentPage(value)}
+			color="primary"
+		/>
         </div>
           <div className="events-list-container">
 			{loading ? <h1>Loading</h1>:
